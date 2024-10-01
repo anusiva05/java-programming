1762b04e2c75dc7654867342b4e990cff3f3fbd3
Anulya <sudhasiva4023@gmail.com>
2024-10-01 16:04:22 +0530

    Add files via upload

---
+++ celcius.java
@@ -0,0 +1,8 @@
+public class celcius {
+    public static void main(String args[]){
+        float celcius,Fahrenheit ;
+        Fahrenheit=20;
+        celcius=((Fahrenheit-32)*5/9);
+        System.out.print("Temperature in celcius:"+celcius);
+    }   
+}
